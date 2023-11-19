e3a07297b029f6b368ffbb8ee41e9ea336a9ba56
inot65 <tft_toni@hotmail.com>
2023-11-19 16:37:12 +0200

    finalizare app

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const conectareMongoDb = async ()=> {
   try {
     await mongoose.connect(process.env.MONGO_URL);
       console.log('Conectat la baza de date!');
-    
   } catch (error) {
     console.log(error)
   }
@@ -26,63 +25,48 @@ const conectareMongoDb = async ()=> {
 }
 conectareMongoDb();
 
-// try {
-//   mongoose.connect(process.env.MONGO_URL);
-//   console.log('Conectat la baza de date!');
+const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, 'images');
+      },
+      filename: (req, file, cb) => {
+        cb(null, req.body.name);
+      },
+    });
 
-//   const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//       cb(null, 'images');
-//     },
-//     filename: (req, file, cb) => {
-//       cb(null, req.body.name);
-//     },
-//   });
 
 //   // pentru incarcarea unui singur fisier
-//   const upload = multer({storage: storage});
-//   app.post('/api/upload', upload.single('file'), (req, res) => {
-//     res.status(200).json('Fisierul a fost incarcat!');
-//   });
-//   // locatia unde sunt stocate fisiere imagine
-//   app.use('/images', express.static(path.join(__dirname, '/images')));
+  const upload = multer({storage: storage});
+  app.post('/api/upload', upload.single('file'), (req, res) => {
+    res.status(200).json('Fisierul a fost incarcat!');
+  });
+  // locatia unde sunt stocate fisiere imagine
+  app.use('/images', express.static(path.join(__dirname, '/images')));
 
-//   app.use('/api/auth', authRoute);
-//   app.use('/api/users', userRoute);
-//   app.use('/api/posts', postRoute);
-//   app.use('/api/categories', categoryRoute);
-//   app.get('/', (req, res) => {
-//     res.send('My API running ! 🥳')
-//   })
-
-//   app.listen('5000', () => {
-//     // pornesc serverul
-//     console.log('Backend-ul ruleaza pe portul 5000...');
-//   });
-// } catch (error) {
-//   console.log(error.message);
-// }
-
-// // Export the Express API
-// module.exports = app;
+  app.use('/api/auth', authRoute);
+  app.use('/api/users', userRoute);
+  app.use('/api/posts', postRoute);
+  app.use('/api/categories', categoryRoute);
 
 
-// index.js
-// const express = require('express')
+const PORT = 5000
 
-const PORT = 4000
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} ${process.env.MONGO_URL}`)
-})
-
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
-
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+try {
+  app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT}`)
+  })
+  
+  app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳 ', process.env.MONGO_URL)
+  })
+  
+  app.get('/about', (req, res) => {
+    res.send('This is my about route..... ')
+  })
+  
+} catch (error) {
+  console.log(error)
+}
 
 // Export the Express API
 module.exports = app
