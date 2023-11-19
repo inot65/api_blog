35fa5505b6e79e0a45751f538941e42aaa657aa6
inot65 <tft_toni@hotmail.com>
2023-11-19 15:38:29 +0200

    de test

--- index.js
+++ index.js
@@ -1,55 +1,77 @@
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const authRoute = require('./routes/auth');
-const userRoute = require('./routes/users');
-const postRoute = require('./routes/posts');
-const categoryRoute = require('./routes/categories');
-const multer = require('multer');
-const path = require('path');
+// const express = require('express');
+// const app = express();
+// const dotenv = require('dotenv');
+// const mongoose = require('mongoose');
+// const authRoute = require('./routes/auth');
+// const userRoute = require('./routes/users');
+// const postRoute = require('./routes/posts');
+// const categoryRoute = require('./routes/categories');
+// const multer = require('multer');
+// const path = require('path');
 
-dotenv.config();
+// dotenv.config();
 
-// permite sa trimita datele din body in format JSON
-app.use(express.json());
+// // permite sa trimita datele din body in format JSON
+// app.use(express.json());
 
-try {
-  mongoose.connect(process.env.MONGO_URL);
-  console.log('Conectat la baza de date!');
+// try {
+//   mongoose.connect(process.env.MONGO_URL);
+//   console.log('Conectat la baza de date!');
 
-  const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'images');
-    },
-    filename: (req, file, cb) => {
-      cb(null, req.body.name);
-    },
-  });
+//   const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, 'images');
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null, req.body.name);
+//     },
+//   });
 
-  // pentru incarcarea unui singur fisier
-  const upload = multer({storage: storage});
-  app.post('/api/upload', upload.single('file'), (req, res) => {
-    res.status(200).json('Fisierul a fost incarcat!');
-  });
-  // locatia unde sunt stocate fisiere imagine
-  app.use('/images', express.static(path.join(__dirname, '/images')));
+//   // pentru incarcarea unui singur fisier
+//   const upload = multer({storage: storage});
+//   app.post('/api/upload', upload.single('file'), (req, res) => {
+//     res.status(200).json('Fisierul a fost incarcat!');
+//   });
+//   // locatia unde sunt stocate fisiere imagine
+//   app.use('/images', express.static(path.join(__dirname, '/images')));
 
-  app.use('/api/auth', authRoute);
-  app.use('/api/users', userRoute);
-  app.use('/api/posts', postRoute);
-  app.use('/api/categories', categoryRoute);
-  app.get('/', (req, res) => {
-    res.send('My API running ! 🥳')
-  })
+//   app.use('/api/auth', authRoute);
+//   app.use('/api/users', userRoute);
+//   app.use('/api/posts', postRoute);
+//   app.use('/api/categories', categoryRoute);
+//   app.get('/', (req, res) => {
+//     res.send('My API running ! 🥳')
+//   })
 
-  app.listen('5000', () => {
-    // pornesc serverul
-    console.log('Backend-ul ruleaza pe portul 5000...');
-  });
-} catch (error) {
-  console.log(error.message);
-}
+//   app.listen('5000', () => {
+//     // pornesc serverul
+//     console.log('Backend-ul ruleaza pe portul 5000...');
+//   });
+// } catch (error) {
+//   console.log(error.message);
+// }
+
+// // Export the Express API
+// module.exports = app;
+
+
+// index.js
+const express = require('express')
+
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
 
 // Export the Express API
-module.exports = app;
+module.exports = app
